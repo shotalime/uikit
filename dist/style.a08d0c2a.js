159179f46e0b3107fd4f3e77987a9d5b762f973d
Evgeniy Nikitin <shotalime@mail.ru>
2019-04-19 07:30:19 +0300

    Сhanged file structure

--- dist/style.a08d0c2a.js
+++ dist/style.a08d0c2a.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../Users/Jaguar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"saas/style.sass":[function(require,module,exports) {
+},{"./bundle-url":"../../../Users/Jaguar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"style.sass":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64080" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59015" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../Users/Jaguar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.cb3fd2d1.js.map
+//# sourceMappingURL=/style.a08d0c2a.js.map
