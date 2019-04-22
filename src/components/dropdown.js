4492ef7fc99c52a046412c5f6ad56734de9ca346
Evgeniy Nikitin <shotalime@mail.ru>
2019-04-22 20:55:03 +0300

    some changes in the template files

---
+++ src/components/dropdown.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$('.dropdown__btn-minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	
+	$('.dropdown__btn-plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
+	});
+	
+});
