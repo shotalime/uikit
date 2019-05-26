c0463d29e907c7a8fa8206b9db3bc646d68e69e6
Evgeniy Nikitin <shotalime@mail.ru>
2019-05-26 20:16:41 +0300

    checkbox buttons block added

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,10 +1,10 @@
 jQuery(function($) {
-    $(".expandable-checkbox-list__items").hide();
+    $(".expandable-checkbox-list").find(".checkbox-buttons").hide();
 
     $(".expandable-checkbox-list__button").on('click', function() {
-        $(".expandable-checkbox-list__items").toggle();
+        $(".expandable-checkbox-list").find(".checkbox-buttons").toggle();
         var value= $(".expandable-checkbox-list__arrow").text();
-        if (value ="keyboard_arrow_down") {
+        if (value == "keyboard_arrow_down") {
             $(".expandable-checkbox-list__arrow").text("keyboard_arrow_up");
         } else {
             $(".expandable-checkbox-list__arrow").text("keyboard_arrow_down");
