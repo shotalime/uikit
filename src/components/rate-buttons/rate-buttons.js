8348236eb4e46e82784448808c94dd8bd2e54c7f
Evgeniy Nikitin <shotalime@mail.ru>
2019-05-31 19:16:20 +0300

    added rate buttons

---
+++ src/components/rate-buttons/rate-buttons.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+    $(".rate-button__item").on("click", function() {
+        $(this).text("star");
+        $(this).prevAll(".rate-button__item").text("star");
+        $(this).nextAll(".rate-button__item").text("star_border");
+    })
+});
