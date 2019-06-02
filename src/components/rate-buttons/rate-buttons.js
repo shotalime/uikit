7493ede4df0f6951d0e6e9c8a350b3bb0f185e27
Evgeniy Nikitin <shotalime@mail.ru>
2019-06-02 13:31:49 +0300

    fixed rate buttons

--- src/components/rate-buttons/rate-buttons.js
+++ src/components/rate-buttons/rate-buttons.js
@@ -1,7 +1,7 @@
-jQuery(function($) {
-    $(".rate-button__item").on("click", function() {
-        $(this).text("star");
-        $(this).prevAll(".rate-button__item").text("star");
-        $(this).nextAll(".rate-button__item").text("star_border");
-    })
-});
+// jQuery(function($) {
+//     $(".rate-button__item").on("click", function() {
+//         $(this).text("star");
+//         $(this).prevAll(".rate-button__item").text("star");
+//         $(this).nextAll(".rate-button__item").text("star_border");
+//     })
+// });
