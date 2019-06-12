a82d62be2741fda59671dcbab51190135821224b
Evgeniy Nikitin <shotalime@mail.ru>
2019-06-12 13:01:33 +0300

    added pagination element, fixed fonts and some elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 
 var jquery = require("jquery");
-window.$ = window.jQuery = jquery; // notice the definition of global variables here
+window.$ = window.jQuery = jquery;
 require("/jQuery/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js");
-// require("jquery-ui-dist/jquery-ui.js");
-//- <script src="/jQuery/jquery.maskedinput.js"></script>
+
 var mask = require('jquery-mask-plugin');
-// $.mask = mask;
-// window.mask = mask;
