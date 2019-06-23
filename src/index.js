de5e7dd40d0e6421856b6f6db24a6393366e5a90
Evgeniy Nikitin <shotalime@mail.ru>
2019-06-23 08:48:08 +0300

    rename like-button and rate-button

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 require("/jQuery/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js");
-
 var mask = require('jquery-mask-plugin');
+
+
