b490d2b5a5c2c9f289e445853bbc33e767856d32
Evgeniy Nikitin <shotalime@mail.ru>
2019-07-05 17:41:10 +0300

    Added slick plugin.  Made slideshow to index.html

---
+++ src/components/slider.js
@@ -0,0 +1,15 @@
+
+        $(function() {
+            $('.index__slider').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                arrows: false,
+                variableWidth: true,
+                centerMode: true
+            });
+        });
+
+  
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 require("/jQuery/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js");
+require('/jQuery/slick-1.8.1/slick/slick.min.js')
 var mask = require('jquery-mask-plugin');
 
 
