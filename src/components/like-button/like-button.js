27d94beaa1946a195c8a2e4326a6f8a5bb73b413
Evgeniy Nikitin <shotalime@mail.ru>
2019-07-10 19:08:27 +0300

    Clicked state added to like-button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+
+
+
+    
+    var button = $('.like-button__button')
+  
+        if($(button).hasClass('clicked')){ 
+  
+            $('.clicked').find(".material-icons").text("favorite");
+            $('.clicked').addClass("like-button__button_clicked");
+            $('.clicked').parent().addClass("like-button_clicked");            
+        }
+
+    
     $('.like-button__button').click(function () {
+        
+        
+
         if($(this).find(".material-icons").html() != "favorite") {
             var $input = $(this).find('.like-button__count');
             var count = parseInt($input.text()) + 1;
