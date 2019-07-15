a6704d8cfdec683d52f2959ff87f68cab74b30ba
Evgeniy Nikitin <shotalime@mail.ru>
2019-07-15 19:35:42 +0300

    Action added to form

--- src/components/room-selection/room-selection.js
+++ src/components/room-selection/room-selection.js
@@ -8,12 +8,4 @@ jQuery(function($) {
         $('.room-selection .datepicker__wrap').toggleClass('datepicker__wrap-hide');
 
     });
-    // временно закоментировано, для проверки ПП
-    // $(document).click(function(event) {
-    //     if ($(event.target).closest(".datepicker__wrap").length) return;
-    //     if ($(event.target).closest(".room-selection .date-dropdown").length) return;
-    //     $(".datepicker__wrap").addClass('datepicker__wrap-hide')
-    //     event.stopPropagation();
-    //   });
-    
 });
